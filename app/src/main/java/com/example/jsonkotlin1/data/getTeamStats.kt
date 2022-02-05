90a6d793821af2711359a7a630ece27f078603a9
Jingyan Sun <jsun2008@163.com>
2022-02-04 16:23:40 -0800

    Update getTeamStats.kt

--- app/src/main/java/com/example/jsonkotlin1/data/getTeamStats.kt
+++ app/src/main/java/com/example/jsonkotlin1/data/getTeamStats.kt
@@ -2,8 +2,24 @@ package com.example.jsonkotlin1.data
 
 import com.example.jsonkotlin1.data.db.entity.FootballTeam
 
+enum class Result{
+    WIN, LOSS, DRAW
+}
+
+
+fun update(list: List<FootballTeam>, teamId: String, result: Result){
+
+
+    
+}
+
 fun getTeamStats(fetchedFootballStats: FootballStats): List<FootballTeam>{
     val list: List<FootballTeam> = mutableListOf()
+    fetchedFootballStats.forEach {
+        // Use teamId because two teams could have the same name
 
+
+
+    }
     return list
 }
